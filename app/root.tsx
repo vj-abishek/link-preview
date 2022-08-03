3f922ac213c2b95476e970da00d05346477279f2
Abishek PY <43115551+vj-abishek@users.noreply.github.com>
2022-08-03 15:31:23 +0000

    feat: v1

--- app/root.tsx
+++ app/root.tsx
@@ -11,7 +11,7 @@ import styles from "./styles/app.css"
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Preview your social links in one click",
   viewport: "width=device-width,initial-scale=1",
 });
 
